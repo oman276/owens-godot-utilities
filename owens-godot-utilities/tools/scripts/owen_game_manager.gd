fce8c206a2611e4f08bdfcd52c9ac29bbc0b2684
Owen Gallagher <owengallagher276@gmail.com>
2025-10-26 12:45:44 -0400

    Mouse Reticle, level loading

--- owens-godot-utilities/tools/scripts/owen_game_manager.gd
+++ owens-godot-utilities/tools/scripts/owen_game_manager.gd
@@ -3,10 +3,11 @@ class_name OwenGameManager
 
 # OwenGameManager
 # A simple game manager for handling level loading and global game state.
-# version 1.0
-# last updated: 2025-09-28
+# version 1.0.0
+# last updated: 2025-10-26
 
 # Note: This class works best as a singleton (autoload) so it can be accessed globally.
+# Some other utilities may depend on this being a singleton, accessible under the game "GameManager".
 
 ## Game State is a globally queriable enum for the current state of the game.
 ## You can use this to check if the game is paused, loading, etc to restrict or enable certain features.
@@ -33,22 +34,29 @@ var level_path_dict : Dictionary = {
 ## These variables control the loading screen and basic behavior. 
 ## Meant to be used with my Loading utiliities, but you can implement your own loading screen logic if you want.
 
-# Loading is set to off by default. Enable if if you have the rest of the loading screen implemented.
+# Loading is set to off by default. Enable if if you have the rest of the loading screen system implemented.
 var uses_loading_screen : bool = false
 ## This is the canvas layer that will be used for the loading screen.
 var loading_canvas : CanvasLayer = null
 ## This is the string path to the loading canvas scene. Make sure to replace it yourself.
+## TODO: Add Loading System
 var loading_canvas_path : String = ""
+# Time to wait before actually loading the level, to allow for fade in effects.
 var loading_time : float = 0.1
 
-@export var initial_level : Levels = Levels.NONE
+# The initial level to load when the game starts.
+var initial_level : Levels = Levels.NONE
+# The current global game state. The initial state is DEFAULT.
 var current_global_state : GameState = GameState.DEFAULT
+# The current level enum. Set to NONE by default. 
+# Note that at the start of the game, the manager will load the initial_level instead.
 var current_level : Levels = Levels.NONE
+# The current level node instance, which we reference so we can unload it later.
 var current_level_node : Node2D = null
 
 # Mouse Reticle
-# Define a custom mouse reticle to replace the system cursor.
-
+# We can define a custom mouse reticle to replace the system cursor.
+var mouse_reticle_path : String = "res://owens-godot-utilities/tools/scenes/OwenMouseReticle.tscn"
 # Set to true if you want to use a custom mouse reticle and have the mouse reticle setup.
 var load_custom_mouse : bool = false
 # The mouse reticle node to be saved here.
@@ -61,13 +69,11 @@ func _ready():
 		else:
 			loading_canvas = load(loading_canvas_path).instantiate()
 			add_child(loading_canvas)
-
 	
-	# todo: move this into the Mouse Reticle utility?
-	#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-	#mouse_reticle_tscn = load("res://scenes/mouse_reticle.tscn")
-	#mouse_reticle = mouse_reticle_tscn.instantiate()
-	#add_child(mouse_reticle)
+	if load_custom_mouse:
+		var mouse_reticle_tscn = load(mouse_reticle_path)
+		mouse_reticle = mouse_reticle_tscn.instantiate()
+		add_child(mouse_reticle)
 	
 	load_level(initial_level)
 
@@ -84,28 +90,34 @@ func reload_level() -> void:
 
 ## Unload the current level and load a new one.
 func _force_load_level(new_level: Levels):
-	current_level = new_level
-	loading_canvas.fade_in()
-	await get_tree().create_timer(loading_time).timeout
+	if uses_loading_screen and loading_canvas:
+		loading_canvas.fade_in()
+		await get_tree().create_timer(loading_time).timeout
 
 	if current_level_node:
 		remove_child(current_level_node)
 		current_level_node.queue_free()
 		current_level_node = null
+		_on_unload_level(current_level)
+
+	current_level = new_level
 
 	var scene_str = level_path_dict[new_level]
 	if scene_str != "":
 		var scene = load(scene_str)
 		current_level_node = scene.instantiate()
 		add_child(current_level_node)
-	loading_canvas.fade_out()
 
-func custom_mouse_visible(visible: bool) -> void:
+	_on_load_level(current_level)
+	if uses_loading_screen and loading_canvas:
+		loading_canvas.fade_out()
+
+func custom_mouse_visible(mouse_visibility: bool) -> void:
 	if not load_custom_mouse:
 		push_warning("OwenGameManager: custom_mouse_visible called but load_custom_mouse is not enabled.")
 		return
-	mouse_reticle.visible = visible
-	
+	mouse_reticle.visible = mouse_visibility
+
 func _on_load_level(level : Levels) -> void:
 	# Implement any logic you want to happen when a level is loaded here.
 	# There may be general setup you want to do in all cases, or you can do 
--- owens-godot-utilities/tools/scripts/owen_mouse_reticle.gd
+++ owens-godot-utilities/tools/scripts/owen_mouse_reticle.gd
@@ -1,11 +1,31 @@
 extends Control
 class_name OwenMouseReticle
 
-@onready var mouse_sprite : Sprite2D
+# OwenMouseReticle
+# A simple mouse reticle for 2D games.
+# version 1.0.0
+# last updated: 2025-10-26
+
+# Usage Notes:
+# This is intended to be used with the existing MouseReticle object.
+# You can instance this scene and add it to your UI layer to have a
+# custom mouse reticle per-scene.
+
+# The GameManager can also manage a global mouse reticle if desired,
+# which will be controllable via the GameManager's custom_mouse_visible()
+# function, and will remain persistent across level loads.
+# It will attempt to load the object at a particular path, so make sure
+# to set that path correctly in the GameManager script.
+
+# The sprite used for the mouse reticle.
+@onready var mouse_sprite : Sprite2D = $Sprite2D
+# Locally tracking whether the reticle is active.
 var is_active : bool = false
 
-func _ready():
-	mouse_sprite = $MouseSprite
+func _init():
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 func _process(_delta):
 	position = get_global_mouse_position()
+
+# TODO: Need to actually add the reticle tscn file
