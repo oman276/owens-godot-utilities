564c28953d39a06d82168353c106371331b08c1b
Owen Gallagher <owengallagher276@gmail.com>
2026-01-11 11:10:04 -0500

    wip destruct particles

--- addons/owens-godot-framework/tools/scripts/level-elements/destructibles.gd
+++ addons/owens-godot-framework/tools/scripts/level-elements/destructibles.gd
@@ -1,10 +1,15 @@
 extends RigidBody2D
 class_name OwenDestructible
 
+# OwenDestructible
+# A simple object which can be destroyed when its health reaches zero.
+# version 1.0.0
+# last updated: 2026-01-11
+
 @export var root_node: Node2D = null
 @export var initial_health: float = 100.0
 
-@export var particles_on_destruction: GPUParticles2D = null
+@export var particles_on_destruction: PackedScene = null
 
 var health: float
 
@@ -16,7 +21,13 @@ func reduce_health(amount: float):
     if health <= 0:
         destroy()
 
-func destroy():    
+func destroy():
+    if particles_on_destruction:
+        var particles = particles_on_destruction.instantiate() as OwenOneShotParticle
+        if particles:
+            get_tree().get_current_scene().add_child(particles)
+            particles.global_position = global_position
+
     if root_node:
         root_node.queue_free()
     else:
